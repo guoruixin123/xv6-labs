35f4d0c245eac23b7c5e957431659a532165240b
guoruixin <374534457@qq.com>
2026-04-17 11:13:45 +0800

    my xv6 progress

--- user/cat.c
+++ user/cat.c
@@ -4,8 +4,7 @@
 
 char buf[512];
 
-void
-cat(int fd)
+void cat(int fd)
 {
   int n;
 
@@ -21,8 +20,7 @@ cat(int fd)
   }
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int fd, i;
 
